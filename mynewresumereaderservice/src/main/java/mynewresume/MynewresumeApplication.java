07aea6638628faac03be71103f18f1ae224a3151
Sathish Jayapal <sjayapal@skminfotech.com>
2022-03-05 10:23:19 -0600

    Changes to the DTO mapping

--- mynewresumereaderservice/src/main/java/mynewresume/MynewresumeApplication.java
+++ mynewresumereaderservice/src/main/java/mynewresume/MynewresumeApplication.java
@@ -1,15 +1,19 @@
 package mynewresume;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MynewresumeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MynewresumeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MynewresumeApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- mynewresumereaderservice/src/main/java/mynewresume/controllers/HomeController.java
+++ mynewresumereaderservice/src/main/java/mynewresume/controllers/HomeController.java
@@ -1,17 +1,23 @@
 package mynewresume.controllers;
 
+import mynewresume.dto.ResumeUserProfileDto;
 import mynewresume.exceptions.UserNotFoundException;
 import mynewresume.models.ResumeUserEducation;
 import mynewresume.models.ResumeUserJob;
 import mynewresume.models.ResumeUserProfile;
+import mynewresume.models.SecurityUser;
 import mynewresume.repos.ResumeUserProfileRepo;
+import mynewresume.repos.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,15 +25,33 @@ import java.util.Optional;
 public class HomeController {
     @Autowired
     ResumeUserProfileRepo userProfileRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/")
     public String rootHello() {
+        SecurityUser securityUser = new SecurityUser();
+        securityUser.setUserName("foo");
+        securityUser.setPassword("foo");
+        securityUser.setActive(true);
+        securityUser.setRoles("Active");
+        userRepository.save(securityUser);
+        ResumeUserProfile resumeUserProfile = new ResumeUserProfile();
+        resumeUserProfile.setUserName("foo");
+        resumeUserProfile.setTheme("1");
+        resumeUserProfile.setSummary("Some Summary");
+        resumeUserProfile.setName("Sathish Jayapal");
+        resumeUserProfile.setPhone("60404023456");
+        resumeUserProfile.setGitHubRepo("http://github.com");
+        resumeUserProfile.setTwitterHandle("dotsky");
+        userProfileRepository.save(resumeUserProfile);
         List<ResumeUserProfile> resumeUserProfileList = userProfileRepository.findAll();
+        List<ResumeUserJob> resumeUserJobs = new ArrayList<>();
+        List<ResumeUserEducation> resumeUserEducationList = new ArrayList<>();
         resumeUserProfileList.forEach(data -> {
             ArrayList<String> myJobResp = new ArrayList<>();
             myJobResp.add("First Job");
             myJobResp.add("Second Job");
-
             ResumeUserJob userJob = new ResumeUserJob();
             userJob.setCompany("American Express");
             userJob.setDesignation("Application Development Engineer");
@@ -35,7 +59,6 @@ public class HomeController {
             userJob.setResponsibilities(myJobResp);
             userJob.setStartDate(LocalDate.of(1999, 5, 1));
             userJob.setEndDate(LocalDate.of(2000, 6, 1));
-
             ResumeUserJob userJob2 = new ResumeUserJob();
             userJob2.setCompany("Novell Express");
             userJob2.setDesignation("Application Development Engineer");
@@ -43,11 +66,8 @@ public class HomeController {
             userJob2.setResponsibilities(myJobResp);
             userJob2.setStartDate(LocalDate.of(2000, 6, 1));
             userJob2.setEndDate(LocalDate.of(2002, 10, 1));
-
-            data.getResumeUserJobList().clear();
-            data.getResumeUserJobList().add(userJob);
-            data.getResumeUserJobList().add(userJob2);
-            userProfileRepository.save(data);
+            resumeUserJobs.add(userJob);
+            resumeUserJobs.add(userJob2);
             ResumeUserEducation resumeUserEducation = new ResumeUserEducation();
             resumeUserEducation.setCollege("MIT");
             resumeUserEducation.setQualification("Bachelors Degree");
@@ -55,18 +75,12 @@ public class HomeController {
             resumeUserEducation.setId(1);
             resumeUserEducation.setStartDate(LocalDate.of(2000, 6, 1));
             resumeUserEducation.setEndDate(LocalDate.of(2002, 10, 1));
-            data.getResumeUserEducationList().clear();
-            data.getResumeUserEducationList().add(resumeUserEducation);
+            resumeUserEducationList.add(resumeUserEducation);
+            data.setResumeUserEducationList(resumeUserEducationList);
+            data.setResumeUserJobList(resumeUserJobs);
+            data.setKeySkills(Arrays.asList("JAVA", "iOS"));
             userProfileRepository.save(data);
-
-            data.getKeySkills().clear();
-            data.getKeySkills().add("JAVA");
-            data.getKeySkills().add("iOS");
-
-            userProfileRepository.save(data);
-
         });
-        userProfileRepository.findAll().stream().forEach(data -> System.out.println(data));
         return "hello";
     }
 
@@ -75,11 +89,21 @@ public class HomeController {
         return "user";
     }
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private ResumeUserProfileDto convertToDto(ResumeUserProfile resumeUserProfile) {
+        ResumeUserProfileDto resumeUserProfileDto = modelMapper.map(resumeUserProfile, ResumeUserProfileDto.class);
+        return resumeUserProfileDto;
+    }
+
     @GetMapping("/view/{user}")
-    public ResumeUserProfile viewUser(@PathVariable String user) {
+    @Transactional(readOnly = true)
+    public ResumeUserProfileDto viewUser(@PathVariable String user) {
         Optional<ResumeUserProfile> userProfileOptional = userProfileRepository.findByUserName(user);
         userProfileOptional.orElseThrow(() -> new UserNotFoundException("User Not Found"));
-        userProfileOptional.get().getUserName();
-        return userProfileOptional.get();
+        ResumeUserProfile resumeUserProfile = userProfileOptional.get();
+        ResumeUserProfileDto resumeUserProfileDto = convertToDto(resumeUserProfile);
+        return resumeUserProfileDto;
     }
 }
