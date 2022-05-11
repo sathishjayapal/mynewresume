a12c072d0dce179a702ef9e4963d42d5a1d0dd60
Sathish Jayapal <sjayapal@skminfotech.com>
2022-05-10 23:28:10 -0500

    New Resume Maker Skeleton

---
+++ resume-maker/src/test/java/me/sathish/resumemaker/ResumeMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sathish.resumemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
